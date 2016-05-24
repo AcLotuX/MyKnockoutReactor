f11407f1b1967602e2b6e1a22aec105e17cfdf87
AcLotuX <amct_791@hotmail.com>
2016-05-24 00:35:50 -0500

    Thirth step - observables

--- app/js/models/product.js
+++ app/js/models/product.js
@@ -1,9 +1,9 @@
 var Product = function(id, name, price, stock){
     "use strict";
-    var _id = id,
-        _name = name,
-        _price = price,
-        _stock = stock;
+    var _id = ko.observable(id),
+        _name = ko.observable(name),
+        _price = ko.observable(price),
+        _stock = ko.observable(stock);
     
     return {
         id:_id,
--- app/js/viewmodel.js
+++ app/js/viewmodel.js
@@ -1,4 +1,12 @@
-var vm = {
-    product: Product(1, 'T-Shirt', 10, 20);
-};
-ko.applyBindings(vm);//This how knockout is activated
+//var vm = {
+//    product: Product(1, 'T-Shirt', 10, 20)
+//};
+//ko.applyBindings(vm);//This how knockout is activated
+
+var vm = (function(){
+    var product = Product(1, 'T-Shirt', 10, 20);
+    return {
+        product: product
+    };
+})();
+ko.applyBindings(vm);
